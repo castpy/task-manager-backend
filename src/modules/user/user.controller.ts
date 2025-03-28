81a6612feddcf2429e38cac901bc8a07880ca517
Marcus Castilho <marcus.castilgo.castpy@gmail.com>
2025-03-28 15:34:42 -0300

    feat: new user module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserServices } from './user.service';
-import { ApiBearerAuth, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserAuthGuard } from '../auth/utils/jwr.auth.guard.utils';
 import { Roles, Users } from '@prisma/client';
 import { GetUser } from '../auth/decorators/get.user.decorator';
 import { UserRoles } from '../auth/decorators/role.decorator';
+import { UserDto } from 'src/dtos/user.dto';
 
 @ApiTags('User')
 @ApiBearerAuth()
@@ -15,6 +21,7 @@ export class UserController {
   constructor(private readonly userServices: UserServices) {}
 
   @Get('/me')
+  @ApiOkResponse({ type: UserDto })
   @UserRoles(Roles.ADMIN, Roles.USER)
   async getMe(@GetUser() user: Users) {
     return this.userServices.getMe(user);
