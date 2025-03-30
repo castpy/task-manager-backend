f0c8d941c310e591f7e7c4188820cfdb727407fd
Marcus Castilho <marcus.castilgo.castpy@gmail.com>
2025-03-30 19:30:51 -0300

    feat: adicionar tarefas de exemplo ao seeder de usuários e simplificar as respostas de erro no controlador de usuários

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -34,41 +34,32 @@ import { ErrorResponseDto } from 'src/dtos/error.dto';
 @Controller('user')
 @ApiSecurity('bearer')
 @UseGuards(UserAuthGuard)
+@UserRoles(Roles.ADMIN, Roles.USER)
+@ApiBadRequestResponse({ type: ErrorResponseDto })
+@ApiUnauthorizedResponse({ type: ErrorResponseDto })
 export class UserController {
   constructor(private readonly userServices: UserServices) {}
 
   @Get('/me')
-  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
-  @ApiBadRequestResponse({ type: ErrorResponseDto })
   @ApiOkResponse({ type: UserDto })
-  @UserRoles(Roles.ADMIN, Roles.USER)
   async getMe(@GetUser() user: Users) {
     return this.userServices.getMe(user);
   }
 
   @Get('/tasks')
-  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
-  @ApiBadRequestResponse({ type: ErrorResponseDto })
-  @UserRoles(Roles.ADMIN, Roles.USER)
   @ApiOkResponse({ type: TaskDto, isArray: true })
   async getTasks(@GetUser() user: Users) {
     return this.userServices.getTasks(user);
   }
 
   @Post('/task')
-  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
-  @ApiBadRequestResponse({ type: ErrorResponseDto })
   @ApiBody({ type: NewTaskDto })
-  @UserRoles(Roles.ADMIN, Roles.USER)
   async newTask(@GetUser() user: Users, @Body() data: NewTaskDto) {
     return this.userServices.newTask(data, user);
   }
 
   @Put('/task/:id')
-  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
-  @ApiBadRequestResponse({ type: ErrorResponseDto })
   @ApiBody({ type: NewTaskDto })
-  @UserRoles(Roles.ADMIN, Roles.USER)
   @ApiParam({ name: 'id', type: 'string' })
   async putTask(
     @GetUser() user: Users,
@@ -79,18 +70,12 @@ export class UserController {
   }
 
   @Put('/status-task')
-  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
-  @ApiBadRequestResponse({ type: ErrorResponseDto })
   @ApiBody({ type: PutTaskDto })
-  @UserRoles(Roles.ADMIN, Roles.USER)
   async putStatusTask(@GetUser() user: Users, @Body() data: PutTaskDto) {
     return this.userServices.putStatusTask(data, user);
   }
 
   @Delete('/task/:id')
-  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
-  @ApiBadRequestResponse({ type: ErrorResponseDto })
-  @UserRoles(Roles.ADMIN, Roles.USER)
   @ApiParam({ name: 'id', type: 'string' })
   async deleteTask(@GetUser() user: Users, @Param('id') id: string) {
     return this.userServices.deleteTask(id, user);
